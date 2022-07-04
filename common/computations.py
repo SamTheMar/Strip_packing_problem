84e7da1f09a1c8735cf51871858826fc5a050700
michele98 <calvanese.michele@gmail.com>
2022-07-04 13:03:28 +0200

    refactor: moved optimize and bisection_solve from SAT_utils.py and SMT_Lib_utils.py to computations.py
    docs: wrote docstring for compute_all_instances in computations.py

--- common/computations.py
+++ common/computations.py
@@ -10,11 +10,165 @@ from utils import visualize, read_instance, save_solution, sort_by_area
 import sys
 sys.path.append('../SAT')
 sys.path.append('../SMT')
-from SMT_Lib_utils import SMT_optimize
-from SAT_utils import SAT_optimize
+from SMT_Lib_solver import SMT_Lib_solver
+from SMT_Lib_solver_rotated import SMT_Lib_solver_rotated
+from SAT_solver import SAT_solver
+from SAT_solver_rotated import SAT_solver_rotated
 
 
-def compute_all_instances(mode = "SAT",
+def bisection_solve(W, H_lb, H_ub, rectangles, allow_rotation=False, verbose=True, mode = 'SAT', *args, **kwargs):
+    """
+    Find the optimal height of the strip packing problem using SMT order encoding.
+    The optimization is done using the bisection method.
+
+    Parameters
+    ----------
+    W : int
+        total width of the strip
+    H_lb : int
+        lower bound for the height of the strip
+    H_ub : int
+        upper bound for the height of the strip
+    rectangles : list of namedtuple('Rectangle', ['w', 'h'])
+        A list of rectangles. This contains the width and height of every rectangle.
+    allow_rotation : bool, default False
+        Toggle whether to allow 90 degree rotation of the rectangles.
+    verbose : bool, default True
+    mode : string, default SAT
+        either SAT or SMT.
+    *args, **kwargs
+        all additional arguments are passed to the solver.
+
+    Returns
+    -------
+    H : int
+        optimal height of the strip
+    positioned_rectangles : list of namedtuple('PositionedRectangle', ['x', 'y', 'w', 'h'])
+        A list of rectangles. This contains bottom left x and y coordinate and
+        the width and height of every rectangle.
+    """
+    if mode != 'SAT' and mode != 'SMT':
+        print("ERROR! The mode must either be SAT or SMT!")
+        return
+    i = 0  # counter for logging
+    positioned_rectangles_lastSAT = []  # to keep track of the last SAT height
+
+    if verbose:
+        print("\nUsing bisection method:")
+    while H_lb < H_ub:
+        i += 1
+        if verbose:
+            print(f"\nCycle {i}: \n{H_lb} <= H <= {H_ub}.")
+
+        H = (H_lb + H_ub)//2
+        if verbose:
+            print(f"Trying H = {H}.")
+
+        if mode == "SAT":
+            if allow_rotation:
+                solver = SAT_solver_rotated(W, H_lb, rectangles, *args, **kwargs)
+            else:
+                solver = SAT_solver(W, H_lb, rectangles, *args, **kwargs)
+        else:
+            if allow_rotation:
+                solver = SMT_Lib_solver_rotated(W, H_lb, rectangles, *args, **kwargs)
+            else:
+                solver = SMT_Lib_solver(W, H_lb, rectangles, *args, **kwargs)
+        positioned_rectangles = solver.solve()
+
+        if len(positioned_rectangles) > 0:
+            positioned_rectangles_lastSAT = positioned_rectangles  # save this configuration
+            H_ub = H
+            if verbose:
+                print(f"SAT.")
+        else:
+            H_lb = H + 1
+            if verbose:
+                print(f"UNSAT.")
+
+    # if the last height is UNSAT, use the configuration used for the previous height
+    if H_lb > H:
+        positioned_rectangles = positioned_rectangles_lastSAT
+
+    if verbose:
+        print(f"\nOptimal value: H = {H_lb}.")
+    return H_lb, positioned_rectangles
+
+
+def optimize(W, rectangles, allow_rotation, verbose, mode = 'SAT', *args, **kwargs):
+    """
+    Find the optimal height of the strip packing problem using SMT order encoding.
+    The bisection method is used to optimize the strip height.
+
+    The lower bound for the strip height is given by: ceil(total_area/strip_width),
+    The upper bound is the sum of the heights of the rectangles.
+
+    The lower bound for the height is tried first before the bisection method.
+
+    Parameters
+    ----------
+    W : int
+        total width of the strip
+    rectangles : list of namedtuple('Rectangle', ['w', 'h'])
+        A list of rectangles. This contains the width and height of every rectangle.
+    allow_rotation : bool, default False
+        Toggle whether to allow 90 degree rotation of the rectangles.
+    verbose : bool, default True
+    mode : string, default SAT
+        either SAT or SMT.
+    *args, **kwargs
+        all additional arguments are passed to the solver.
+
+    Returns
+    -------
+    H : int
+        optimal height of the strip
+    rectangles : list of namedtuple('PositionedRectangle', ['x', 'y', 'w', 'h'])
+        A list of rectangles. This contains bottom left x and y coordinate and
+        the width and height of every rectangle.
+    """
+    if mode != 'SAT' and mode != 'SMT':
+        print("ERROR! The mode must either be SAT or SMT!")
+        return
+    total_area = sum([r.h*r.w for r in rectangles])
+    H_ub = sum([r.h for r in rectangles]) // 2
+
+    H_lb = int(np.ceil((total_area)/W))
+
+    if verbose:
+        print("Finding optimal strip height.")
+        print("Initial estimation:")
+        print(f"{H_lb} <= H <= {H_ub}.")
+        print("\nTrying lower bound first.")
+
+    if mode == "SAT":
+        if verbose: print("Solving using SAT.")
+        if allow_rotation:
+            if verbose: print("Rotation allowed.")
+            solver = SAT_solver_rotated(W, H_lb, rectangles, *args, **kwargs)
+        else:
+            if verbose: print("Rotation not allowed.")
+            solver = SAT_solver(W, H_lb, rectangles, *args, **kwargs)
+    else:
+        if verbose: print("Solving using SMT.")
+        if allow_rotation:
+            if verbose: print("Rotation allowed.")
+            solver = SMT_Lib_solver_rotated(W, H_lb, rectangles, *args, **kwargs)
+        else:
+            if verbose: print("Rotation not allowed.")
+            solver = SMT_Lib_solver(W, H_lb, rectangles, *args, **kwargs)
+
+    positioned_rectangles = solver.solve()
+    if len(positioned_rectangles) > 0:
+        if verbose: print(f"Lower bound is SAT.\nOptimal value: H = {H_lb}.")
+        return H_lb, positioned_rectangles
+
+    if verbose:
+        print("Lower bound is UNSAT.")
+    return bisection_solve(W, H_lb+1, H_ub, rectangles, allow_rotation, verbose, *args, **kwargs)
+
+
+def compute_all_instances(mode = 'SAT',
                             input_range=range(40),
                             plot=True,
                             save_to_file=True,
@@ -32,10 +186,31 @@ def compute_all_instances(mode = "SAT",
 
     Parameters
     ----------
-    mode : string
+    mode : string, default 'SAT'
         either SAT or SMT.
-    ...todo
-
+    input_range : range or list of int
+        instances to compute.
+    plot : bool, default True
+        if True, plot the results as images.
+    save_to_file : bool, default True
+        if True, save the computation results and the resulting plots.
+    verbose : bool, default True
+    allow_rotation : bool, default False
+        if True, allow rotation of the rectangles.
+    sorting_by_area : bool, default True
+        if True, sort rectangles be area before passing them to the solver.
+    break_symmetries : bool, default True
+        if True, let the solver break symmetries.
+    solver : string, default 'z3'
+        Underlying solver to use, only z3 and cvc5 are currently supported. Only used if mode = 'SMT'.
+    logi : string, default 'LIA'
+         Underlying SMT-LIB logic to use. Only used if mode = 'SMT'.
+    timeout : int, default 300
+        timeout of each computation in seconds.
+    plot_kw : dict
+        addidtional keywork arguments passed to ``visualize``
+    plot_output_format : string, default 'png'
+        Output figure format. Only used if save_to_file is True.
     """
     if mode != 'SAT' and mode != 'SMT':
         print("ERROR! The mode must either be SAT or SMT!")
@@ -82,9 +257,9 @@ def compute_all_instances(mode = "SAT",
         try:
             start = time.time()
             if mode == "SAT":
-                H, positioned_rectangles = SAT_optimize(W, rectangles, allow_rotation, verbose, break_symmetries, timeout=timeout)
+                H, positioned_rectangles = optimize(W, rectangles, allow_rotation, verbose, mode, break_symmetries, timeout=timeout)
             else:
-                H, positioned_rectangles = SMT_optimize(W, rectangles, allow_rotation, verbose, break_symmetries, solver=solver, logic=logic, timeout=timeout)
+                H, positioned_rectangles = optimize(W, rectangles, allow_rotation, verbose, mode, break_symmetries, solver=solver, logic=logic, timeout=timeout)
             execution_time = time.time() - start
             print("\nFound height:", H)
             print(f"Execution time in seconds: {execution_time:.3f}")
