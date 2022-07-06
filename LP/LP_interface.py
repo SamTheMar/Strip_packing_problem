745f1496607dd398800ea7bb99a16fa8099f7d1d
DavideFemia <femiadavide97@gmail.com>
2022-07-06 18:57:53 +0200

    added new rotation model for lp

--- LP/LP_interface.py
+++ LP/LP_interface.py
@@ -1,6 +1,7 @@
+#from curses.textpad import rectangle
 import sys
 sys.path.append('./')
-from common.utils import read_instance,save_solution,PositionedRectangle,write_execution_time
+from common.utils import read_instance,save_solution,PositionedRectangle,Rectangle,write_execution_time
 import math
 import numpy as np
 
@@ -58,26 +59,62 @@ def validPositions(rectangles,W,H, verbose=False):
 
     return V,C
 
-def solve_LP(instance_num, ordering = True):
+def createAssoc(rectangles,H,W):
+    notAssoc = set()
+    assoc = []
+    new_circuits = []
+    for i in range(len(rectangles)):
+        r = rectangles[i]
+        if r.w == r.h or r.w > H or r.h > W:
+            notAssoc.add(i+1)
+        else:
+            rassoc = [j+1 for j in range(len(rectangles)) if j!=i and r.w==rectangles[j].h and  r.h==rectangles[j].w]
+            if rassoc and rassoc[0]>i+1:
+                    assoc.append([i+1,rassoc[0],2])
+            elif not(rassoc):
+                new_circuits.append(Rectangle(r.h,r.w))
+                assoc.append([i+1,len(rectangles)+len(new_circuits),1])
+    rectangles.extend(new_circuits)
+    return np.array(assoc),notAssoc
+    
+
+
+def solve_LP(instance_num, ordering = False, rotation = True):
     
     input_url = './instances/ins-%d.txt'%instance_num
-    output_url = "./LP/out/no_rotation/txt/ins-%d-sol.txt"%instance_num
+    if rotation:
+        output_url = "./LP/out/rotation/txt/ins-%d-sol.txt"%instance_num
+    else:
+        output_url = "./LP/out/no_rotation/txt/ins-%d-sol.txt"%instance_num
 
     W, n, rectangles = read_instance(input_url)
-
-    if ordering:
-        rectangles = [rectangles[i] for i in np.argsort([r.w*r.h for r in rectangles])[::-1]]
 
     H = math.ceil(sum([rectangles[i].w * rectangles[i].h for i in range(n)])/W)-1
 
     timeout = timedelta(seconds=300) #300
 
-    model = Model("./LP/VLSI_LP.mzn") # modify search and restart strtegy in the model file
+    if rotation:
+        model = Model("./LP/VLSI_rotation_LP.mzn")
+    else:
+        model = Model("./LP/VLSI_LP.mzn")
     solver = Solver.lookup('gurobi') # coin-bc, cbc, coinbc
 
     instance = Instance(solver, model)
     instance["W"] = W
-    instance["nCircuits"] = n
+    if rotation:
+        assoc,notAssoc = createAssoc(rectangles,H,W)
+        #print(rectangles)
+        #print(assoc)
+        #print(notAssoc)
+        instance["nCircuits"] = len(rectangles)
+        instance["nAssoc"] = assoc.shape[0]
+        instance["assoc"] = assoc
+        instance["notAssoc"] = notAssoc
+    else:
+        instance["nCircuits"] = n
+
+    if ordering:
+        rectangles = [rectangles[i] for i in np.argsort([r.w*r.h for r in rectangles])[::-1]]
 
     start_time = time()
     feasible = False
@@ -88,6 +125,8 @@ def solve_LP(instance_num, ordering = True):
         H = H + 1
 
         V, C = validPositions(rectangles,W,H)
+        #print(H)
+        #print(V)
 
         #print(filterValidPositionBC(rectangles,V,W,H))
         
@@ -97,8 +136,9 @@ def solve_LP(instance_num, ordering = True):
         instance["C"] = C
         instance["V"] = [0 if i<0 else V[i][-1] for i in range(-1,len(rectangles))]
         #instance["V"] = V
-        instance["bc"] = 1 if ordering else np.argmax([r.w*r.h for r in rectangles])+1
-        instance["filteredPosBC"] = set(filterValidPositionBC(rectangles,V,W,H))
+        if not(rotation):
+            instance["bc"] = 1 if ordering else np.argmax([r.w*r.h for r in rectangles])+1
+            instance["filteredPosBC"] = set(filterValidPositionBC(rectangles,V,W,H))
 
         inner_start_time = time()
         result = instance.solve(timeout=timeout)
@@ -113,11 +153,19 @@ def solve_LP(instance_num, ordering = True):
         x = result.solution.x
         positioned_rectangles = []
         for i in range(len(rectangles)):
-            vpos = x[i].index(1)
-            index = vpos + 1 - V[i][0]
-            #index = vpos - V[i-1]
-            pos = (index % ((W - rectangles[i].w)+1), H - index // ((W - rectangles[i].w)+1) - rectangles[i].h)
-            positioned_rectangles.append(PositionedRectangle(pos[0],pos[1],rectangles[i].w,rectangles[i].h))
+            if rotation and not(1 in x[i]):
+                continue
+            if rotation:
+                indexes = [j+1-V[i][0] for j in range(len(x[i])) if x[i][j]==1]
+                pos = [(idx % ((W - rectangles[i].w)+1), H - idx // ((W - rectangles[i].w)+1) - rectangles[i].h) for idx in indexes]
+                new_rectangles = [PositionedRectangle(p[0],p[1],rectangles[i].w,rectangles[i].h) for p in pos]
+                positioned_rectangles.extend(new_rectangles)
+            else:
+                vpos = x[i].index(1)
+                index = vpos + 1 - V[i][0]
+                #index = vpos - V[i-1]
+                pos = (index % ((W - rectangles[i].w)+1), H - index // ((W - rectangles[i].w)+1) - rectangles[i].h)
+                positioned_rectangles.append(PositionedRectangle(pos[0],pos[1],rectangles[i].w,rectangles[i].h))
 
             #print(rectangles[i], pos)
 
@@ -127,5 +175,5 @@ def solve_LP(instance_num, ordering = True):
     else:
         print(instance_num,"not solved")
 
-for instance_num in range(1,2):
+for instance_num in range(1,40):
     solve_LP(instance_num)
