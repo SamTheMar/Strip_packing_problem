0b60c3e5f46eeab315064fca648cbd584d189a6b
DavideFemia <femiadavide97@gmail.com>
2022-07-08 15:53:49 +0200

    modified python interface to gather CP_results

--- CP/CP_interface.py
+++ CP/CP_interface.py
@@ -33,48 +33,79 @@ solver = Solver.lookup("chuffed")
 
 time_tables = []
 
-for instance_num in range(1,2):
-    #input_url = "./CP/CP_instances/ins-%d.dzn"%instance_num
-    input_url = './instances/ins-%d.txt'%instance_num
-    if rotation:
-        output_url = "./CP/CP_solutions_rotation/ins-%d-sol.txt"%instance_num
-    else:
-        output_url = "./CP/CP_solutions/ins-%d-sol.txt"%instance_num
+for ss in range(1, 4):
+    for rs in range(1,4):
+        for instance_num in range(1,41):
+            #input_url = "./CP/CP_instances/ins-%d.dzn"%instance_num
+            input_url = './instances/ins-%d.txt'%instance_num
+            output_url = './CP/out'
+            if rotation:
+                output_url = output_url + '/rotation'
+                if rs == 1:
+                    output_url = output_url + '/luby'
+                elif rs == 2:
+                    output_url = output_url + '/geometric'
+                else:
+                    output_url = output_url + '/no_restart'
+                if ss == 1:
+                    output_url = output_url + '/dom_w_deg'
+                elif ss == 2:
+                    output_url = output_url + '/impact'
+                else:
+                    output_url = output_url + '/non_increasing_area'
+                output_url = output_url + '/ins-%d-sol.txt'%instance_num
+            else:
+                output_url = output_url + '/no_rotation'
+                if rs == 1:
+                    output_url = output_url + '/luby'
+                elif rs == 2:
+                    output_url = output_url + '/geometric'
+                else:
+                    output_url = output_url + '/no_restart'
+                if ss == 1:
+                    output_url = output_url + '/dom_w_deg'
+                elif ss == 2:
+                    output_url = output_url + '/impact'
+                else:
+                    output_url = output_url + '/non_increasing_area'
+                output_url = output_url + '/ins-%d-sol.txt'%instance_num
 
-    instance = Instance(solver, model)
-    # instance.add_file(input_url)
+            instance = Instance(solver, model)
+            # instance.add_file(input_url)
 
-    W, n, rectangles = read_instance(input_url)
+            W, n, rectangles = read_instance(input_url)
 
-    if ordering:
-        rectangles = [rectangles[i] for i in np.argsort([r.w*r.h for r in rectangles])[::-1]]
+            if ordering:
+                rectangles = [rectangles[i] for i in np.argsort([r.w*r.h for r in rectangles])[::-1]]
 
-    bc = np.argmax([r.w*r.h for r in rectangles])
-    sbc = np.argmax([rectangles[i].w*rectangles[i].h if i!=bc else 0 for i in range(len(rectangles))])
+            bc = np.argmax([r.w*r.h for r in rectangles])
+            sbc = np.argmax([rectangles[i].w*rectangles[i].h if i!=bc else 0 for i in range(len(rectangles))])
 
-    minH = math.ceil(sum([r.w*r.h for r in rectangles])/W)
-    maxDimX = max([r.w for r in rectangles])
-    maxH = math.ceil(sum([maxDimX*r.h for r in rectangles])/W)
+            minH = math.ceil(sum([r.w*r.h for r in rectangles])/W)
+            maxDimX = max([r.w for r in rectangles])
+            maxH = math.ceil(sum([maxDimX*r.h for r in rectangles])/W)
 
-    instance["w"] = W
-    instance["n"] = n
-    instance["minH"] = minH
-    instance["maxH"] = maxH
-    instance["bc"] = bc+1
-    instance["sbc"] = sbc+1
-    instance["dimX"] = [r.w for r in rectangles]
-    instance["dimY"] = [r.h for r in rectangles]
+            instance["w"] = W
+            instance["n"] = n
+            instance["minH"] = minH
+            instance["maxH"] = maxH
+            instance["bc"] = bc+1
+            instance["sbc"] = sbc+1
+            instance["dimX"] = [r.w for r in rectangles]
+            instance["dimY"] = [r.h for r in rectangles]
+            instance["search_heuristic"] = ss # 1 dom_deg, 2 impact, 3 input_order
+            instance["restart_strategy"] = rs # 1 luby(150), 2 geometric(2,50), 3 niente
 
-    try:
-        start_time = time()
-        result = instance.solve(timeout=timeout)
-        execution_time = time()-start_time
-        time_tables.append(execution_time)
+            try:
+                start_time = time()
+                result = instance.solve(timeout=timeout)
+                execution_time = time()-start_time
+                time_tables.append(execution_time)
 
-        with open(output_url, "w") as f:
-            f.write(str(result))
-        write_execution_time(output_url, execution_time)
+                with open(output_url, "w") as f:
+                    f.write(str(result))
+                write_execution_time(output_url, execution_time)
 
-        print(instance_num,"solved")
-    except:
-        print(instance_num,"not solved")
+                print(instance_num,"solved")
+            except:
+                print(instance_num,"not solved")
